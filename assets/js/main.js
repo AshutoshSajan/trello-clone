1816cf43b5b95c77e1dc7d83d003a363133e9773
sajan <sajanloveu123@gmail.com>
2019-02-24 19:20:27 +0530

    second commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 var card = document.querySelector(".card-container");
-
+var board = document.querySelector(".board");
+var mainInput;
+var html;
 class Trello {
 	constructor(element, name, data){
 		// this.name = name;
@@ -15,29 +17,40 @@ class Trello {
 	}
 
 	static createCard(e){
-		Trello.defaultList();
-		card.innerHTML = 
+		html = 
 		`<div class="list-container">
 			<input class="main-input" type="text" />
-			<div>
+			<div class="list-footer">
 				<button class="addList">+ Add list</button>
 				<span class="fas fa-times"></span>
 			</div>
 		</div>`;
-		var mainInput =  document.querySelector(".main-input");
+		card.innerHTML = html;
+		mainInput =  document.querySelector(".main-input");
 		mainInput.addEventListener("click", Trello.createList);
+		var addList = document.querySelector(".addList");
+		addList.addEventListener("click", Trello.createList);
 		// mainInput.focus();
 		// mainInput.addEventListener("blur", handleBlur);
 	}
 
 	static createList(e){
-		Trello.defaultList(); 
+		if(mainInput.value.trim()){
+			// card.innerHTML = html;
+			var div = document.createElement("div");
+			// // div.classList.add("list-container");
+			div.innerHTML = card.innerHTML;
+			card.appendChild(div);
+		}
+		
 	}
 }
 
 function handleBlur(e){
 	Trello.defaultList();
 }
+
+Trello.defaultList();
 
 // class List {
 // 	constructor(element, name, data){
